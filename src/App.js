cb19d10e1a7ce8ce2c0a86d414c813f3387807d7
Moab Kidbrooke <michael@mboateng.com>
2025-12-14 23:11:33 +0000

    bug fixes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         This is a test
+          Learn React
         </a>
       </header>
     </div>
